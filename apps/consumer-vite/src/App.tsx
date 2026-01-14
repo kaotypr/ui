cc494133a7442426776d167f1b4fdd634e54cd07
Adhitya Sanjaya <adhityasnjy@gmail.com>
2026-01-15 02:07:26 +0700

    feat(consumer-vite): implement demo components
    - Replaced the initial app structure with a main layout featuring DemoContainerCard components.
    - Added AccordionDemo and AlertDialogDemo components to showcase UI functionality.
    - Introduced DemoContainerCard component to encapsulate demo content with titles and descriptions.

--- apps/consumer-vite/src/App.tsx
+++ apps/consumer-vite/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import DemoContainerCard from './components/DemoContainerCard'
+import { AccordionDemo } from './demos/AccordionDemo'
+import { AlertDialogDemo } from './demos/AlertDialogDemo'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main className="p-4 flex flex-col gap-4 items-center justify-center">
+      <h1 className="text-2xl font-bold">Vite + Kaotypr UI</h1>
+      <DemoContainerCard
+        title="Accordion"
+        description="An accordion is a vertically stacked set of panels, only one of which can be expanded at a time."
+        content={<AccordionDemo />}
+      />
+      <DemoContainerCard
+        title="Alert Dialog"
+        description="A dialog is a modal that appears in front of other content to focus the user's attention on important information."
+        content={<AlertDialogDemo />}
+      />
+    </main>
   )
 }
 
