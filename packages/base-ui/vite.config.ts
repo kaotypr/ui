82acfb6a2299650a1904edb0d502379ef9a6542e
Adhitya Sanjaya <adhityasnjy@gmail.com>
2026-01-22 12:48:53 +0700

    feat(base-ui): setup shadcn (base-ui)

--- packages/base-ui/vite.config.ts
+++ packages/base-ui/vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react-swc'
+import path, { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const dirname =
+	typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+		alias: {
+			"~": resolve(dirname, "src/"),
+		},
+	},
 })
